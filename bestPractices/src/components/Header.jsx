1e119e109781ef6d8df545c74da4d8a50a5a813e
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2024-12-12 14:12:19 +0200

    added propTypes

--- bestPractices/src/components/Header.jsx
+++ bestPractices/src/components/Header.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Header({title}) {
     return <header className="header">{title}</header>;
 }
+
+Header.propTypes = {
+    title: PropTypes.string
+  }
+  
+  Header.defaultProps = {
+    title: 'React App'
+  }
+  
