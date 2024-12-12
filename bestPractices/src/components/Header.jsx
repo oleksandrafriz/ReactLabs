a87e99f53221b8251e4464f1453d8c069f434bed
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2024-12-12 17:36:26 +0200

    added some content

--- bestPractices/src/components/Header.jsx
+++ bestPractices/src/components/Header.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './Header.module.css'
 
-export default function Header({title}) {
-    return <header className="header">{title}</header>;
+export default function Header({ title, links }) {
+  return (
+      <header className={styles.header}>
+          <h1>{title}</h1>
+          <nav>
+              <ul className={styles.navLinks}>
+                  {links.map((link, index) => (
+                      <li key={index}>
+                          <a href={link.href}>{link.label}</a>
+                      </li>
+                  ))}
+              </ul>
+          </nav>
+      </header>
+  );
 }
 
 Header.propTypes = {
-    title: PropTypes.string
-  }
-  
-  Header.defaultProps = {
-    title: 'React App'
-  }
-  
+  title: PropTypes.string,
+  links: PropTypes.arrayOf(
+      PropTypes.shape({
+          href: PropTypes.string,
+          label: PropTypes.string,
+      })
+  ),
+};
+
+Header.defaultProps = {
+  title: 'React App',
+  links: [
+      { href: '#home', label: 'Home' },
+      { href: '#about', label: 'About' },
+      { href: '#contact', label: 'Contact' },
+  ],
+};
